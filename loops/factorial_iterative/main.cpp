b18f331a0aef6061ad31b8536fadb537258c30bb
Lakshyadeep Gogoi <72311817+lakshyadeepgogoi@users.noreply.github.com>
2022-10-09 18:46:18 +0530

    i have updatec

--- loops/factorial_iterative/main.cpp
+++ loops/factorial_iterative/main.cpp
@@ -2,14 +2,24 @@
 using namespace std;
  
 double factorial(int n){
-   //Enter your code here
+       int t1=0;
+    int t2=1;
+    int nextTerm;
+    for(int i=0;i<=n;i++){
+        cout<<t1<<endl;
+        nextTerm=t1+t2;
+        t1=t2;
+        t2=nextTerm;
+
+    }
+    return;
 }
 
 //Driver Code
 int main()
 {   
     int n;
+    cout<<"Enter the nth number= ";
     cin>>n;
-    cout<<factorial(n);
-    return 0;
+    fibb(n);
 }
